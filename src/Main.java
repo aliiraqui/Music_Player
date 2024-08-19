d55347c213a5a801e84df2b8a7776bcd066acb63
Ali Ahmed Iraqui <aliiraqui@hotmail.com>
2024-08-19 20:18:44 +0530

    Created Main Class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import controller.MusicController;
+
+public class Main {
+    public static void main(String[] args) {
+        MusicController controller = new MusicController();
+        controller.start();
+    }
+}
